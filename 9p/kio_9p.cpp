44c9fa4e86121217b6c6f875b2472df85e11b706
Arusekk <floss@arusekk.pl>
2024-10-02 12:03:20 +0200

    9p: new protocol

---
+++ 9p/kio_9p.cpp
@@ -0,0 +1,508 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Arkadiusz Kozdra <floss@arusekk.pl>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kio_9p.h"
+
+#include <QCoreApplication>
+#include <QTcpSocket>
+
+#include "kio_9p_debug.h"
+#include "kio_9p_trace_debug.h"
+
+// Pseudo plugin class to embed meta data
+class KIOPluginForMetaData : public QObject
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.kde.kio.worker.9p" FILE "9p.json")
+};
+
+using namespace KIO;
+
+extern "C" Q_DECL_EXPORT int kdemain(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral("kio_9p"));
+
+    qCDebug(KIO_9P_LOG) << "Starting";
+
+    if (argc != 4) {
+        fprintf(stderr, "Usage: kio_9p protocol domain-socket1 domain-socket2\n");
+        exit(-1);
+    }
+
+    P9Worker worker(argv[2], argv[3]);
+    worker.dispatchLoop();
+
+    qCDebug(KIO_9P_LOG) << "Done";
+    return 0;
+}
+
+class P9DataStream : public QDataStream
+{
+public:
+    P9DataStream(const QByteArray &inp)
+        : QDataStream(inp)
+    {
+        setByteOrder(QDataStream::LittleEndian);
+    }
+    P9DataStream(QByteArray *outp)
+        : QDataStream(outp, QIODevice::WriteOnly)
+    {
+        setByteOrder(QDataStream::LittleEndian);
+    }
+    P9DataStream &operator<<(quint8 i)
+    {
+        *static_cast<QDataStream *>(this) << i;
+        return *this;
+    }
+    P9DataStream &operator>>(quint8 &i)
+    {
+        *static_cast<QDataStream *>(this) >> i;
+        return *this;
+    }
+    P9DataStream &operator<<(quint16 i)
+    {
+        *static_cast<QDataStream *>(this) << i;
+        return *this;
+    }
+    P9DataStream &operator>>(quint16 &i)
+    {
+        *static_cast<QDataStream *>(this) >> i;
+        return *this;
+    }
+    P9DataStream &operator<<(quint32 i)
+    {
+        *static_cast<QDataStream *>(this) << i;
+        return *this;
+    }
+    P9DataStream &operator>>(quint32 &i)
+    {
+        *static_cast<QDataStream *>(this) >> i;
+        return *this;
+    }
+    P9DataStream &operator<<(quint64 i)
+    {
+        *static_cast<QDataStream *>(this) << i;
+        return *this;
+    }
+    P9DataStream &operator>>(quint64 &i)
+    {
+        *static_cast<QDataStream *>(this) >> i;
+        return *this;
+    }
+    P9DataStream &operator<<(QString s)
+    {
+        QByteArray b = s.toUtf8();
+        *this << (quint16)b.size();
+        return *this << b;
+    }
+    P9DataStream &operator>>(QString &s)
+    {
+        quint16 n;
+        *this >> n;
+        QByteArray b(n, Qt::Initialization());
+        *this >> b;
+        s = QString::fromUtf8(b);
+        return *this;
+    }
+    P9DataStream &operator<<(const QByteArray &b)
+    {
+        writeRawData(b.data(), b.size());
+        return *this;
+    }
+    P9DataStream &operator>>(QByteArray &b)
+    {
+        readRawData(b.data(), b.size());
+        return *this;
+    }
+    P9DataStream &operator>>(P9Worker::p9qid &qid)
+    {
+        *this >> qid.qid_type >> qid.qid_version >> qid.qid_path;
+        return *this;
+    }
+    P9DataStream &operator>>(P9Worker::p9statbuf &buf)
+    {
+        quint16 n;
+        *this >> n;
+        QByteArray b(n, Qt::Initialization());
+        *this >> b;
+
+        P9DataStream sub(b);
+        sub >> buf.type >> buf.dev;
+        sub >> buf.qid;
+        sub >> buf.mode;
+        sub >> buf.atime >> buf.mtime;
+        sub >> buf.length;
+        sub >> buf.name;
+        sub >> buf.uid >> buf.gid >> buf.muid;
+        return *this;
+    }
+    P9DataStream &operator>>(UDSEntry &entry)
+    {
+        P9Worker::p9statbuf buf;
+        *this >> buf;
+        entry.clear();
+        entry.reserve(10);
+        entry.fastInsert(KIO::UDSEntry::UDS_NAME, buf.name);
+        entry.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, (buf.mode & P9Worker::DMDIR) ? QT_STAT_DIR : QT_STAT_REG);
+        entry.fastInsert(KIO::UDSEntry::UDS_ACCESS, buf.mode & 0777);
+        entry.fastInsert(KIO::UDSEntry::UDS_ACCESS_TIME, buf.atime);
+        entry.fastInsert(KIO::UDSEntry::UDS_MODIFICATION_TIME, buf.mtime);
+        entry.fastInsert(KIO::UDSEntry::UDS_SIZE, buf.length);
+        entry.fastInsert(KIO::UDSEntry::UDS_USER, buf.uid);
+        entry.fastInsert(KIO::UDSEntry::UDS_GROUP, buf.gid);
+        return *this;
+    }
+};
+
+//===============================================================================
+// P9Worker
+//===============================================================================
+
+P9Worker::P9Worker(const QByteArray &pool, const QByteArray &app)
+    : WorkerBase(QByteArrayLiteral("9p"), pool, app)
+{
+}
+
+void P9Worker::setHost(const QString &host, quint16 port, const QString &user, const QString &pass)
+{
+    if (host != mHost || port != mPort || user != mUser) {
+        mHost = host;
+        mPort = port;
+        mUser = user;
+        if (mSession)
+            closeConnection();
+    }
+}
+
+KIO::WorkerResult P9Worker::openConnection()
+{
+    if (mSession)
+        return Result::pass();
+    mSession = new QTcpSocket;
+    mSession->connectToHost(mHost, mPort ? mPort : 564);
+    qCDebug(KIO_9P_LOG) << "connecting! " << mHost << mPort;
+    mSession->waitForConnected(30 * 1000);
+    qCDebug(KIO_9P_LOG) << "connected!";
+    Result res = negotiateVersion();
+    if (!res.success())
+        return res;
+    // authenticate(142, mUser, QByteArrayLiteral(""));
+    mMaxFid = 0;
+    return attach(0, 0xffffffff, mUser.isEmpty() ? "nobody" : mUser, QByteArrayLiteral(""));
+}
+
+KIO::WorkerResult P9Worker::negotiateVersion()
+{
+    QString proto = "9P2000";
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << mMax;
+        ds << proto;
+    }
+    Result res = sendCmd(Tversion, 0xffff, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rversion, 0xffff);
+}
+
+KIO::WorkerResult P9Worker::authenticate(quint32 afid, QString uname, QString aname)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << afid;
+        ds << uname << aname;
+    }
+    Result res = sendCmd(Tauth, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rauth, 0);
+}
+
+KIO::WorkerResult P9Worker::attach(quint32 fid, quint32 afid, QString uname, QString aname)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << fid << afid;
+        ds << uname << aname;
+    }
+    Result res = sendCmd(Tattach, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rattach, 0);
+}
+
+KIO::WorkerResult P9Worker::walk(quint32 fid, quint32 nfid, QStringList walks)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << fid << nfid << (quint16)walks.size();
+        for (QString s : walks)
+            ds << s;
+    }
+    Result res = sendCmd(Twalk, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rwalk, 0);
+}
+
+KIO::WorkerResult P9Worker::read(quint32 fid, quint64 offset, quint32 count)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << fid << offset << count;
+    }
+    Result res = sendCmd(Tread, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rread, 0);
+}
+
+KIO::WorkerResult P9Worker::open(quint32 fid, quint8 mode)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << fid << mode;
+    }
+    Result res = sendCmd(Topen, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Ropen, 0);
+}
+
+KIO::WorkerResult P9Worker::stat(quint32 fid)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << fid;
+    }
+    Result res = sendCmd(Tstat, 0, payload);
+    if (!res.success())
+        return res;
+    return recvCmd(Rstat, 0);
+}
+
+void P9Worker::closeConnection()
+{
+    delete mSession;
+    mSession = nullptr;
+}
+
+KIO::WorkerResult P9Worker::sendCmd(enum p9cmd type, quint16 tag, const QByteArray &cmd)
+{
+    QByteArray payload;
+    {
+        P9DataStream ds(&payload);
+        ds << (quint32)cmd.size() + 7;
+        ds << (quint8)type;
+        ds << tag;
+        ds << cmd;
+    }
+    qCDebug(KIO_9P_TRACE_LOG) << "sending " << payload;
+    qint64 n = mSession->write(payload);
+    if (n == -1)
+        return Result::fail(ERR_WORKER_DEFINED, tr("write: %1").arg(mSession->errorString()));
+    if (n != payload.size())
+        return Result::fail(ERR_WORKER_DEFINED, tr("Short write"));
+    qCDebug(KIO_9P_TRACE_LOG) << "sent";
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::recvCmd(enum p9cmd type, quint16 tag)
+{
+    mSession->waitForReadyRead(30 * 1000);
+    QByteArray head = mSession->read(4);
+    if (head.size() == 0)
+        return Result::fail(ERR_WORKER_DEFINED, tr("read: %1").arg(mSession->errorString()));
+    if (head.size() != 4)
+        return Result::fail(ERR_WORKER_DEFINED, tr("Short read"));
+    quint32 n = head[0] | head[1] << 8 | head[2] << 16 | head[3] << 24;
+    if (n < 7)
+        return Result::fail(ERR_WORKER_DEFINED, tr("Malformed packet"));
+    QByteArray payload = mSession->read(n - 4);
+    qCDebug(KIO_9P_TRACE_LOG) << "recving " << payload;
+    P9DataStream ds(payload);
+
+    quint8 cmd;
+    quint16 theirtag;
+
+    ds >> cmd;
+    ds >> theirtag;
+
+    if (theirtag != tag)
+        return Result::fail(ERR_WORKER_DEFINED, tr("Unexpected response: %1 (expected %2)").arg(theirtag, tag));
+
+    if (cmd == Rerror) {
+        QString ename;
+        ds >> ename;
+        return Result::fail(ERR_WORKER_DEFINED, ename);
+    }
+    if (cmd != type)
+        return Result::fail(ERR_WORKER_DEFINED, tr("Unexpected response: %1 (expected %2)").arg(type, cmd));
+
+    switch (cmd) {
+    case Rversion: {
+        ds >> mMax;
+        QString ver;
+        ds >> ver;
+        if (ver != "9P2000")
+            return Result::fail(ERR_CANNOT_CONNECT, "9P version not implemented: " + ver);
+        break;
+    }
+    case Rattach: {
+        p9qid qid;
+        ds >> qid;
+        break;
+    }
+    case Rstat: {
+        UDSEntry entry;
+        ds >> entry;
+        statEntry(entry);
+        break;
+    }
+    case Ropen: {
+        p9qid qid;
+        quint32 iounit;
+        ds >> qid >> iounit;
+        if (qid.qid_type & 0x80)
+            mimeType("inode/directory");
+        else
+            mimeType("application/octet-stream");
+        mIsDir = qid.qid_type & 0x80;
+        break;
+    }
+    case Rwalk: {
+        quint16 nqids;
+        ds >> nqids;
+        // nqids < nwalks indicates partial success
+        // need to walk to returned depth and walk the offending component again to get error cause
+        for (quint16 i = 0; i < nqids; i++) {
+            p9qid qid;
+            ds >> qid;
+        }
+        break;
+    }
+    case Rread: {
+        quint32 count;
+        ds >> count;
+        QByteArray filedata(count, Qt::Initialization());
+        ds >> filedata;
+        if (mIsDir) {
+            P9DataStream ds2(filedata);
+            UDSEntry entry;
+            while (!ds2.atEnd()) {
+                ds2 >> entry;
+                listEntry(entry);
+            }
+        } else
+            data(filedata);
+        break;
+    }
+    }
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::stat(const QUrl &url)
+{
+    Result res = openConnection();
+    if (!res.success())
+        return res;
+    quint32 fid = ++mMaxFid;
+    res = walk(0, fid, url.path().mid(1).split('/'));
+    if (!res.success())
+        return res;
+
+    return stat(fid);
+}
+
+KIO::WorkerResult P9Worker::listDir(const QUrl &url)
+{
+    Result res = openConnection();
+    if (!res.success())
+        return res;
+    quint32 fid = ++mMaxFid;
+    res = walk(0, fid, url.path().mid(1).split('/'));
+    if (!res.success())
+        return res;
+
+    res = open(fid, OREAD);
+    if (!res.success())
+        return res;
+
+    return read(fid, 0, 8192);
+}
+
+KIO::WorkerResult P9Worker::mkdir(const QUrl &url, int permissions)
+{
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::rename(const QUrl &src, const QUrl &dst, JobFlags flags)
+{
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::del(const QUrl &url, bool isfile)
+{
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::chmod(const QUrl &url, int permissions)
+{
+    return Result::pass();
+}
+
+KIO::WorkerResult P9Worker::get(const QUrl &url)
+{
+    Result res = openConnection();
+    if (!res.success())
+        return res;
+    quint32 fid = ++mMaxFid;
+    res = walk(0, fid, url.path().mid(1).split('/'));
+    if (!res.success())
+        return res;
+
+    res = open(fid, OREAD);
+    if (!res.success())
+        return res;
+
+    return read(fid, 0, 8192);
+}
+
+KIO::WorkerResult P9Worker::put(const QUrl &url, int permissions, JobFlags flags)
+{
+    return Result::pass();
+}
+
+void P9Worker::worker_status()
+{
+}
+
+KIO::WorkerResult P9Worker::copy(const QUrl &src, const QUrl &dest, int permissions, JobFlags flags)
+{
+    return Result::pass();
+}
+
+QDebug operator<<(QDebug dbg, const Result &r)
+
+{
+    QDebugStateSaver saver(dbg);
+    dbg.nospace() << "Result("
+                  << "success=" << r.success() << ", err=" << r.error() << ", str=" << r.errorString() << ')';
+    return dbg;
+}
+
+// needed for JSON file embedding
+#include "kio_9p.moc"
+
+#include "moc_kio_9p.cpp"
---
+++ 9p/kio_9p.h
@@ -0,0 +1,150 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Arkadiusz Kozdra <floss@arusekk.pl>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#ifndef __kio_9p_h__
+#define __kio_9p_h__
+
+#include <KIO/Global>
+#include <KIO/WorkerBase>
+
+#include <QUrl>
+
+class QTcpSocket;
+
+using Result = KIO::WorkerResult;
+
+class P9Worker : public QObject, public KIO::WorkerBase
+{
+    Q_OBJECT
+public:
+    explicit P9Worker(const QByteArray &poolSocket, const QByteArray &appSocket);
+    Q_DISABLE_COPY_MOVE(P9Worker)
+
+    void setHost(const QString &h, quint16 port, const QString &user, const QString &pass) override;
+    Q_REQUIRED_RESULT Result get(const QUrl &url) override;
+    Q_REQUIRED_RESULT Result listDir(const QUrl &url) override;
+    Q_REQUIRED_RESULT Result stat(const QUrl &url) override;
+    Q_REQUIRED_RESULT Result copy(const QUrl &src, const QUrl &dest, int permissions, KIO::JobFlags flags) override;
+    Q_REQUIRED_RESULT Result put(const QUrl &url, int permissions, KIO::JobFlags flags) override;
+    void closeConnection() override;
+    void worker_status() override;
+    Q_REQUIRED_RESULT Result del(const QUrl &url, bool isfile) override;
+    Q_REQUIRED_RESULT Result chmod(const QUrl &url, int permissions) override;
+    Q_REQUIRED_RESULT Result rename(const QUrl &src, const QUrl &dest, KIO::JobFlags flags) override;
+    Q_REQUIRED_RESULT Result mkdir(const QUrl &url, int permissions) override;
+    Q_REQUIRED_RESULT Result openConnection() override;
+
+#if 0
+    // KIO::FileJob interface
+    Q_REQUIRED_RESULT Result open(const QUrl &url, QIODevice::OpenMode mode) override;
+    Q_REQUIRED_RESULT Result read(KIO::filesize_t size) override;
+    Q_REQUIRED_RESULT Result write(const QByteArray &data) override;
+    Q_REQUIRED_RESULT Result seek(KIO::filesize_t offset) override;
+    Q_REQUIRED_RESULT Result truncate(KIO::filesize_t length) override;
+    Q_REQUIRED_RESULT Result close() override;
+    Q_REQUIRED_RESULT Result special(const QByteArray &data) override;
+#endif
+
+    // Must call after construction!
+    // Bit rubbish, but we need to return something on init.
+    Q_REQUIRED_RESULT Result init();
+    Q_REQUIRED_RESULT Result negotiateVersion();
+    Q_REQUIRED_RESULT Result authenticate(quint32 afid, QString uname, QString aname);
+    Q_REQUIRED_RESULT Result attach(quint32 afid, quint32 fid, QString uname, QString aname);
+    Q_REQUIRED_RESULT Result walk(quint32 fid, quint32 nfid, QStringList walks);
+    Q_REQUIRED_RESULT Result read(quint32 fid, quint64 offset, quint32 count);
+    Q_REQUIRED_RESULT Result open(quint32 fid, quint8 mode);
+    Q_REQUIRED_RESULT Result stat(quint32 fid);
+
+private: // Private variables
+    enum p9cmd {
+        Tversion = 100,
+        Rversion = 101,
+        Tauth = 102,
+        Rauth = 103,
+        Tattach = 104,
+        Rattach = 105,
+        Rerror = 107,
+        Twalk = 110,
+        Rwalk = 111,
+        Topen = 112,
+        Ropen = 113,
+        Tread = 116,
+        Rread = 117,
+        Tclunk = 120,
+        Rclunk = 121,
+        Tstat = 124,
+        Rstat = 125,
+    };
+    enum omode {
+        OREAD = 0,
+        OWRITE = 1,
+        ORDWR = 2,
+        OEXEC = 3,
+        OTRUNC = 0x10,
+        ORCLOSE = 0x40,
+    };
+    enum dmmode {
+        DMDIR = 0x80000000,
+        DMAPPEND = 0x40000000,
+        DMEXCL = 0x20000000,
+        DMTMP = 0x04000000,
+    };
+    struct p9qid {
+        quint8 qid_type;
+        quint32 qid_version;
+        quint64 qid_path;
+    };
+    struct p9statbuf {
+        quint16 type;
+        quint32 dev;
+        p9qid qid;
+        quint32 mode;
+        quint32 atime;
+        quint32 mtime;
+        quint64 length;
+        QString name;
+        QString uid;
+        QString gid;
+        QString muid;
+    };
+    /** True if worker is connected to 9p server. */
+    bool mConnected = false;
+
+    /** Host we are connected to. */
+    QString mHost;
+
+    /** Port we are connected to. */
+    int mPort = -1;
+
+    /** Current user. */
+    QString mUser;
+
+    /** The tcp session for the connection */
+    QTcpSocket *mSession = nullptr;
+
+    /** Maximum message size. */
+    quint32 mMax = 0x20018;
+
+    /** Current top file id. */
+    quint32 mMaxFid = 0;
+
+    /** If open URL is dir */
+    bool mIsDir;
+
+    /** The open URL */
+    QUrl mOpenUrl;
+
+    // KIO::FileJob interface
+    KIO::filesize_t openOffset = 0;
+
+    Q_REQUIRED_RESULT Result sendCmd(enum p9cmd cmd, quint16 tag, const QByteArray &payload);
+    Q_REQUIRED_RESULT Result recvCmd(enum p9cmd cmd, quint16 tag);
+
+    friend class P9DataStream;
+};
+
+#endif
